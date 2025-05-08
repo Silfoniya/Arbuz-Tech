8dc6fc9c7e4ef48add24d18619282fee0187bc4c
Silfoniya <itaksoidet00@yandex.ru>
2025-05-08 20:17:23 +0500

    Полная замена файлов

---
+++ app.js
@@ -0,0 +1,119 @@
+const items = [
+    {
+        id:1,
+        name:"Иркутский холодильник",
+        price:13500,
+        quantity:11,
+        rating:0,
+        producer:"нн подвал",
+        img:""
+    },
+    {
+        id:2,
+        name:"Смартфон VIVO",
+        price:9800,
+        quantity:42,
+        rating:2,
+        producer:"VIVO",
+        img:""
+    },
+    {
+        id:3,
+        name:"Смартфон POCO C4",
+        price:32999,
+        quantity:8,
+        rating:4,
+        producer:"POCO",
+        img:""
+    },
+    {
+        id:4,
+        name:"Смартфон Xiaomi E12 Pro Mini Plus",
+        price:11100,
+        quantity:5,
+        rating:5,
+        producer:"Xiaomi",
+        img:""
+    },
+    {
+        id:5,
+        name:"Процессор Intel Core i5",
+        price:12000,
+        quantity:21,
+        rating:5,
+        producer:"Intel",
+        img:""
+    },
+    {
+        id:6,
+        name:"Процессор Ryzen 7",
+        price:13500,
+        quantity:54,
+        rating:5,
+        producer:"AMD",
+        img:""
+    },
+    {
+        id:7,
+        name:"Видеокарта NVidia RTX 4060 Ti",
+        price:53999,
+        quantity:2,
+        rating:5,
+        producer:"NVidia",
+        img:""
+    },
+        {
+        id:8,
+        name:"Видеокарта Radeon RX 8600",
+        price:53999,
+        quantity:1,
+        rating:5,
+        producer:"Radeon",
+        img:""
+    },
+    {
+        id:9,
+        name:"Видеокарта Radeon RX 7600",
+        price:36000,
+        quantity:0,
+        rating:5,
+        producer:"Radeon",
+        img:""
+    },
+    {
+        id:10,
+        name:"утюг",
+        price:9999,
+        quantity:100,
+        rating:5,
+        producer:"нету они сами у нас заспавнились",
+        img:""
+    },
+    {
+        id:11,
+        name:"сборка ПК - 'дешевая'",
+        price:49999,
+        quantity:3,
+        rating:5,
+        producer:"МЫ",
+        img:""
+    },
+    {
+        id:12,
+        name:"сборка ПК - 'подороже'",
+        price:499999,
+        quantity:1,
+        rating:5,
+        producer:"МЫ",
+        img:""
+    },
+{
+        id:13,
+        name:"Электросамовар",
+        price:5999,
+        quantity:9,
+        rating:5,
+        producer:"Наши программисты",
+        img:""
+    }
+]
